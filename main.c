81f7d0bc97c835770f9b97819be33a19bb54c65b
karem941 <karematef941@gmail.com>
2021-05-02 15:24:36 +0200

    Updated the search function prototype and added the Exit function

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-//surprise!!!
+#include <string.h>
+
 int choise;
 //student struct
 typedef struct Students{
@@ -9,7 +10,7 @@ typedef struct Students{
     int id;
     float grades[6] ;
     char level[10];
-    char email[20];
+    char email[30];
 
 }Student;
 //--------------------------------------------------------------------------
@@ -26,8 +27,8 @@ struct Node {
   Student item;
   struct Node* next;
 };
-struct node *head = NULL;
-struct node *current = NULL;
+struct Node *head = NULL;
+struct Node *current = NULL;
 //insert node at the End of the linked list
 void insert(Student data){};
 //delete item from the linked list
@@ -78,9 +79,61 @@ void save_in_File(){};
 //to sort the file in Alphabetical order
 void sort_file(FILE *ptr){};
 //to add student to linked list then save it in the file
-void Add_Student(){};
+void Add_Student()
+{
+    system("cls");
+    system("color 6");
+    system("title Adding New Student .........");
+
+    Student add, cheek;
+    int level_choise = 0;
+    char fname[15],s_id[10];
+
+
+    do{
+        system("cls");
+        printf("\xB10 Choose The Student Level:\n\t\t 1- Level 1\n\t\t 2- Level 2\n\t\t 3- Level 3\n\t\t 4- Level 4\n\xB10 Enter Choise : ");
+        scanf("%d",&level_choise);
+    }while(level_choise>4 || level_choise<1);
+
+    printf("\nEnter Student Id :");
+    scanf("%d",&add.id);
+    /*
+        if the id exists or not
+    */
+    printf("\nEnter Student Name : ");
+    fflush(stdin);
+    gets(add.name);
+    printf("\nEnter Student Age :");
+    scanf("%d",&add.age);
+
+    printf("\nEnter The student Grades  (if there is no grades yet put Zeros ) :\nSubject 1: ");
+    scanf("%f",&add.grades[0]);
+    printf("Subject 2: ");
+    scanf("%f",&add.grades[1]);
+     printf("Subject 3: ");
+    scanf("%f",&add.grades[2]);
+     printf("Subject 4: ");
+    scanf("%f",&add.grades[3]);
+     printf("Subject 5: ");
+    scanf("%f",&add.grades[4]);
+     printf("Subject 6: ");
+    scanf("%f",&add.grades[5]);
+
+
+    for(int i=0;add.name[i]!=' ';i++)
+    {
+        fname[i]=add.name[i];
+    }
+     strcpy(add.email,fname);
+     sprintf(s_id,"%d",add.id);
+     strcat(add.email,s_id);
+     strcat(add.email,"@winter.com");
+     delay("Saving.");
+
+};
 //to search in the files for a student info
-void search(){};
+void search(FILE * ptr,int key){};
 //updates student info form the file
 void Update_Student_Info(){};
 //view student info form the file
@@ -95,14 +148,15 @@ void Exit()
     system("cls");
     system("color 4");
     system("title Program Ended");
-    Loading("Ending");
+    delay("Ending.");
     printf("\n\n\n\n\t\t\t\t This C Program Was developed  By :\n\n\n\t\t\t\t\t\xB10 AbdElrahman Khaled\n\n\t\t\t\t\t\xB10 Hager Mohamed\n\n\t\t\t\t\t\xB10 Karem Atef\n\n\t\t\t\t\t\xB10 Mohamed AbdElaziz\n\n\t\t\t\t\t\xB10 Mohamed Adel\n\n\n\n\n\n\n\n");
     sleep(5);
 };
 //To delay nicely
-void Loading(char word[15])
+void delay(char word[15])
 {
-    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t%s",word);
+    system("cls");
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t%s",word);
     sleep(1);
     printf(".");
      sleep(1);
